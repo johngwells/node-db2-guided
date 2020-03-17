257a30d926d068a2a2a09f04f4d2b3b9a2e7703d
John Wells <molekewl2017@gmail.com>
2020-03-17 10:09:07 -0700

    move knex confiq to knexfile

--- data/connection.js
+++ data/connection.js
@@ -1,25 +1,7 @@
 const knex = require('knex');
-// object: original
-/*
-const knexConfig = knex({
-  client: 'sqlite3', //db driver
-  connection: {
-    filename: './data/produce.db3' // connection could be an object or a string
-  },
-  useNullAsDefault: true // only needed for sqlite
-});
-*/
 
-// configured a connection to the database
-const knexConfig = {
-  client: 'sqlite3', //db driver
-  connection: {
-    filename: './data/produce.db3' // connection could be an object or a string
-  },
-  useNullAsDefault: true // only needed for sqlite
-};
+const knexfile = require('../knexfile');
 
-// db represents a live connection to the database
-// const db = knex(knexConfig);
+const knexConfig = knexfile.development;
 
 module.exports = knex(knexConfig);
